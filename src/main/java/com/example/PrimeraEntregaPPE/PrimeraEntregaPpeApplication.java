ebe8721e4a69d988dc459d8d2ab3bc261b8174a4
JuanTabares <juanjeronimotc@gmail.com>
2026-02-11 14:37:51 -0500

    First commit

---
+++ src/main/java/com/example/PrimeraEntregaPPE/PrimeraEntregaPpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrimeraEntregaPPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraEntregaPpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraEntregaPpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PrimeraEntregaPPE/PrimeraEntregaPpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeraEntregaPPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraEntregaPpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
